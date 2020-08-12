e57c9cac4f16dc439458bbdb72b68f4640cfebe7
liwz <liwz@koal.com>
2020-08-12 19:29:12 +0800

    1

---
+++ src/components/HoverUnderlineAnimation.vue
@@ -0,0 +1,57 @@
+<template>
+  <p class="hover-underline-animation">Hover this text to see the effect!</p>
+</template>
+
+<style lang="less">
+:root {
+  --textColor: #0087ca;
+}
+
+.hover-underline-animation {
+  font-size: 20px;
+  position: relative;
+  // overflow: hidden;
+  display: inline-block;
+  color: var(--textColor);
+  &:before {
+    content: "";
+    position: absolute;
+    height: 2px;
+    left: 0;
+    width: 100%;
+    bottom: 0;
+    transform-origin: bottom left;
+    transform: scaleX(0);
+    transition: transform 0.5s ease-out;
+    background-color: var(--textColor);
+  }
+  &:hover:before {
+    transform: scaleX(1);
+    transform-origin: bottom right;
+  }
+}
+// .hover-underline-animation {
+
+//   display: inline-block;
+//   position: relative;
+//   color: var(--textColor);
+//   font-size: 18px;
+//   &:after {
+//     content: "";
+//     position: absolute;
+//     width: 100%;
+//     transform: scaleX(0);
+//     height: 2px;
+//     bottom: 0;
+//     left: 0;
+//     background-color: #0087ca;
+//     transform-origin: bottom right;
+//     transition: transform 0.25s ease-out;
+//   }
+
+//   &:hover:after {
+//     transform: scaleX(1);
+//     transform-origin: bottom left;
+//   }
+// }
+</style>
---
+++ src/components/Images.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="titles">
+    <div class="title">
+      <img src="https://via.placeholder.com/200x150" />
+      <h2>30 Seconds of CSS</h2>
+    </div>
+    <div class="title">
+      <img src="https://via.placeholder.com/200x150" />
+      <h2>30 Seconds of CSS</h2>
+    </div>
+    <div class="title">
+      <img src="https://via.placeholder.com/200x150" />
+      <h2>30 Seconds of CSS</h2>
+    </div>
+  </div>
+</template>
+<script>
+import { inject, ref } from "vue";
+export default {
+  name: "Images",
+  setup() {
+    let imagesRef = ref(false);
+    const InjectObject = inject("provide", {});
+    let handler = InjectObject(imagesRef);
+    // console.log(handler,'handler')
+
+    setTimeout(() => {
+        // console.log('settimeout 2000s')
+        handler.name = 'hippo'
+    }, 2000);
+  },
+};
+</script>
+<style lang="less" scoped>
+.titles {
+  font-size: 0;
+  width: 600px;
+  margin: 0 auto;
+  .title {
+    display: inline-block;
+    width: calc(600px / 3);
+    h2 {
+      font-size: 20px;
+    }
+  }
+}
+</style>
