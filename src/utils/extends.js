1b44d244d38ff89ffce015013d41e2872c55d08b
liwz <liwz@koal.com>
2020-08-17 19:24:09 +0800

    1

--- src/utils/extends.js
+++ src/utils/extends.js
@@ -43,9 +43,196 @@ function createFlow(task) {
     };
 }
 
-createFlow([
-    () => log("a"),
-    () => log("b"),
-    subFlow,
-    [() => delay(1000).then(() => log("d")), () => log("e")],
-]).run(() => console.log("done"));
+// createFlow([
+//     () => log("a"),
+//     () => log("b"),
+//     subFlow,
+//     [() => delay(1000).then(() => log("d")), () => log("e")],
+// ]).run(() => console.log("done"));
+
+class _promise_ {
+    callbacks = [];
+    value = "";
+    status = "pending";
+    constructor(func) {
+        func(
+            value => this._resolve(value),
+            err => this._reject(err)
+        );
+    }
+
+    then(onfulfilled, onrejected) {
+        return new _promise_((resolve, reject) => {
+            this._handle({
+                resolve: resolve,
+                reject: reject,
+                onfulfilled: onfulfilled,
+                onrejected: onrejected,
+            });
+        });
+    }
+
+    _handle(callback) {
+        console.log(callback);
+        if (this.status === "pending") {
+            console.log(this.status, "pending");
+            this.callbacks.push(callback);
+            return;
+        }
+
+        if (this.status === "fulfilled") {
+            if (!callback.onfulfilled) {
+                callback.resolve(this.value);
+                return;
+            }
+            let result = callback.onfulfilled(this.value);
+            callback.resolve(result);
+        }
+
+        if (this.status === "rejected") {
+            if (!callback.onrejected) {
+                callback.reject(this.value);
+                return;
+            }
+
+            let result = callback.onrejected(this.value);
+            callback.reject(result);
+        }
+    }
+
+    _resolve(value) {
+        if (
+            value &&
+            (typeof value === "function" || typeof value === "object")
+        ) {
+            if (typeof value.then === "function") {
+                value.then(
+                    value => this._resolve(value),
+                    err => this._reject(err)
+                );
+            }
+        }
+        if (this.status !== "pending") return;
+        this.status = "fulfilled";
+        this.value = value;
+        this.callbacks.forEach(f => this._handle(f));
+    }
+
+    _reject(errVal) {
+        if (
+            errVal &&
+            (typeof errVal === "function" || typeof errVal === "object")
+        ) {
+            if (typeof errVal.then === "function") {
+                errVal.then(
+                    value => this._resolve(value),
+                    err => _this._reject(err)
+                );
+            }
+        }
+
+        if (this.status !== "pending") return;
+        this.status = "rejected";
+        this.value = errVal;
+        this.callbacks.forEach(f => this._handle(f));
+    }
+    catch(onerror) {
+        return this.then(null, onerror);
+    }
+
+    static resolve(value) {
+        // return new /
+        if (
+            value &&
+            (typeof value === "function" || typeof value === "object")
+        ) {
+            if (typeof value.then === "function") {
+                return value;
+            }
+        } else if (value) {
+            return new _promise_(resolve => resolve(value));
+        } else {
+            return new _promise_(resolve => resolve());
+        }
+    }
+
+    static reject(value) {
+        // return new /
+        if (
+            value &&
+            (typeof value === "function" || typeof value === "object")
+        ) {
+            if (typeof value.then === "function") {
+                return value;
+            }
+        } else if (value) {
+            return new _promise_((resolve, reject) => reject(value));
+        } else {
+            return new _promise_((resolve, reject) => reject(value));
+        }
+    }
+
+    static all(promises) {
+        if (!promises.length) return;
+        let result = [];
+        let promisesLen = promises.length;
+        let count = 0;
+
+        return new _promise_((resolve, reject) => {
+            for (let index = 0; index < promises.length; index++) {
+                promises[index].then(
+                    res => {
+                        count++;
+                        result[index] = res;
+                        if (count === promisesLen) {
+                            resolve(result);
+                        }
+                    },
+                    err => {
+                        reject(err);
+                    }
+                );
+                // const element = array[index];
+            }
+        });
+    }
+
+    static race(promises) {
+        if (!promises.length || !Array.isArray(promises)) return;
+        return new _promise_((resolve, reject) => {
+            for (let index = 0; index < promises.length; index++) {
+                // const element = array[index];
+                promises[index].then(
+                    res => {
+                        resolve(res);
+                    },
+                    err => {
+                        reject(err);
+                    }
+                );
+            }
+        });
+    }
+}
+
+new _promise_((resolve, reject) => {
+    reject(10);
+})
+    .then(
+        res => {
+            console.log(res);
+        },
+        err => {
+            console.log(err);
+            return new _promise_((resolve, reject) => {
+                resolve(1000);
+            });
+        }
+    )
+    .then(res => {
+        console.log(res);
+        return new _promise_((resolve, reject) => {
+            reject(3000);
+        });
+    })
+    .catch(console.log);
