eff5b24059fd5064f3f1632d477798e67148d21c
bigHippoZz <289901948@qq.com>
2020-09-01 13:51:57 +0800

    添加loading组件

---
+++ src/library/Loading-hoc.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>loading...</div>
+</template>
+
+<script>
+export default {
+    name: "Loading",
+    setup(props) {
+        return {};
+    },
+};
+</script>
+
+<style lang="less" scoped></style>
