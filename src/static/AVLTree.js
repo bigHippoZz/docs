c314ce771f78fe8f8b8ca47ecbc4fa25f1e9912d
bighippo <289901948@qq.com>
2020-09-30 11:22:15 +0800

    平衡树

--- src/static/AVLTree.js
+++ src/static/AVLTree.js
@@ -1,5 +1,12 @@
 import { BinarySearchTree } from "./BinarySearchTree";
 
+const BalanceFactor = {
+    UNBALANCED_RIGHT: 1,
+    SLIGHTLY_UNBALANCED_RIGHT: 2,
+    BALANCED: 3,
+    SLIGHTLY_UNBALANCED_LEFT: 4,
+    UNBALANCED_LEFT: 5,
+};
 class AVLTree extends BinarySearchTree {
     getNodeHeight(node) {
         if (node === null) {
@@ -9,11 +16,39 @@ class AVLTree extends BinarySearchTree {
         const right = this.getNodeHeight(node.right);
         return Math.max(left, right) + 1;
     }
+    getBalanceFactor(node) {
+        const heightDifference =
+            this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+        switch (heightDifference) {
+            case value:
+                break;
+            default:
+                break;
+        }
+    }
+    getBalanceFactor(node) {
+        const heightDifference =
+            this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+        switch (heightDifference) {
+            case -2:
+                return BalanceFactor.UNBALANCED_RIGHT;
+            case -1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+            case 1:
+                return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+            case 2:
+                return BalanceFactor.UNBALANCED_LEFT;
+            default:
+                return BalanceFactor.BALANCED;
+        }
+    }
 }
 
 const tree = new AVLTree();
 
-tree.insert(10);
-tree.insert(8);
+tree.insert(3);
+tree.insert(2);
+tree.insert(1);
 console.log(tree);
+
 console.log(tree.getNodeHeight(tree.root));
