c0b7eef04dbee717a8604898d04f5f39454bcf99
DESKTOP-00J5IIA\Vuer <289901948@qq.com>
2020-11-10 17:52:51 +0800

    1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -106,86 +106,83 @@ class CarBuilder implements Builder {
   }
 }
 
-
 class Singleton {
   public static instance: Singleton;
   private constructor(...args: unknown[]) {
-    console.log(...args)
+    console.log(...args);
   }
   public static getInstance(...args: unknown[]): Singleton {
     if (!Singleton.instance) {
-      const result = new Singleton
-      Singleton.instance = result
-      return result
+      const result = new Singleton();
+      Singleton.instance = result;
+      return result;
     }
     return Singleton.instance;
   }
 }
 
-const s = Singleton.getInstance()
-const a = Singleton.getInstance()
-// console.log(a===s)
+const s = Singleton.getInstance();
+const a = Singleton.getInstance();
 
-/**
- * The Target defines the domain-specific interface used by the client code.
- */
 class Target {
   public request(): string {
-      return 'Target: The default target\'s behavior.';
+    return 'Target: The default target\'s behavior.';
   }
 }
 
-/**
-* The Adaptee contains some useful behavior, but its interface is incompatible
-* with the existing client code. The Adaptee needs some adaptation before the
-* client code can use it.
-*/
 class Adaptee {
   public specificRequest(): string {
-      return '.eetpadA eht fo roivaheb laicepS';
+    return ".eetpadA eht fo roivaheb laicepS";
   }
 }
 
-/**
-* The Adapter makes the Adaptee's interface compatible with the Target's
-* interface.
-*/
 class Adapter extends Target {
-  private adaptee: Adaptee;
-
-  constructor(adaptee: Adaptee) {
-      super();
-      this.adaptee = adaptee;
+  constructor(private adaptee: Adaptee) {
+    super();
   }
 
   public request(): string {
-      const result = this.adaptee.specificRequest().split('').reverse().join('');
-      return `Adapter: (TRANSLATED) ${result}`;
+    const result = this.adaptee
+      .specificRequest()
+      .split("")
+      .reverse()
+      .join("");
+    return `Adapter: (TRANSLATED) ${result}`;
   }
 }
 
-/**
-* The client code supports all classes that follow the Target interface.
-*/
-function clientCode(target: Target) {
-  console.log(target.request());
-}
-
-console.log('Client: I can work just fine with the Target objects:');
 const target = new Target();
-clientCode(target);
-
-console.log('');
 
 const adaptee = new Adaptee();
-console.log('Client: The Adaptee class has a weird interface. See, I don\'t understand it:');
-console.log(`Adaptee: ${adaptee.specificRequest()}`);
 
-console.log('');
-
-console.log('Client: But I can work with it via the Adapter:');
 const adapter = new Adapter(adaptee);
-clientCode(adapter);
 
+// 不兼容的对象放入适配类中
+// console.log(adapter.request(),'adapter')
+
+interface Implementation {
+  operationImplementation(): string;
+}
+
+class ConcreteImplementationA implements Implementation {
+  operationImplementation() {
+    return 'ConcreteImplementationA: Here\'s the result on the platform A.';
+  }
+}
+class ConcreteImplementationB implements Implementation {
+  operationImplementation() {
+    return 'ConcreteImplementationA: Here\'s the result on the platform B.';
+  }
+}
+// 使用变量保存多个维度的变化
+class Abstraction {
+  private abstract: Implementation;
+  constructor(abstract: Implementation) {
+    this.abstract = abstract;
+  }
+  public operations(): string {
+    return this.abstract.operationImplementation()
+  }
+}
 
 
