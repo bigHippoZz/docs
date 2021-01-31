2670fb0247888dea3a182ae4473a0630c7428ac4
bighippo <289901948@qq.com>
2021-01-31 14:49:56 +0800

    47. 全排列 II 最终题解

--- src/algorithm/backTracking/47. 全排列 II.js
+++ src/algorithm/backTracking/47. 全排列 II.js
@@ -25,55 +25,10 @@
  * @return {number[][]}
  */
 const permuteUnique = function(nums) {
-  // const result = [];
-  // const len = nums.length;
-  // const used = new Array(len);
-  // nums.sort((a, b) => a - b);
-
-  // function helper(paths) {
-  //   if (paths.length === len) {
-  //     result.push(paths.slice());
-  //     return;
-  //   }
-
-  //   for (let i = 0; i < len; i++) {
-
-  //     // 进行剪枝
-  //     // console.log(!used[i - 1]);
-
-  //     if (!used[i - 1] && i !== 0) {
-  //       console.log(`used[i - 1]->${i} ->${nums[i]}`);
-  //     }
-  //     if (nums[i - 1] === nums[i] && i && !used[i - 1]) {
-  //       continue;
-  //     }
-
-  //     if (used[i]) {
-  //       continue;
-  //     }
-
-  //     paths.push(nums[i]);
-
-  //     used[i] = true;
-
-  //     helper(paths);
-
-  //     paths.pop();
-
-  //     used[i] = false;
-
-  //   }
-  // }
-
-  // helper([]);
-
-  // return result;
   nums.sort();
-  console.log(`sort -> ${nums}`);
   const result = [];
   const used = [];
   const { length } = nums;
-
   function helper(paths, index) {
     if (index === length) {
       result.push(paths.slice());
@@ -81,9 +36,6 @@ const permuteUnique = function(nums) {
     }
 
     for (let i = 0; i < length; i++) {
-      const preNums = nums[i - 1];
-      const currentNums = nums[i];
-      const preUsed = used[i - 1];
       if (i && nums[i - 1] === nums[i] && !used[i - 1]) {
         continue;
       }
@@ -95,14 +47,11 @@ const permuteUnique = function(nums) {
       helper(paths, index + 1);
       used[i] = false;
       paths.pop();
-      
     }
   }
-
   helper([], 0);
-
   return result;
 };
 
-const result = permuteUnique([1, 3, 1]);
-console.log(result);
+// const result = permuteUnique([1, 3, 1]);
+// console.log(result);
