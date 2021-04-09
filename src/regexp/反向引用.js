b94da65bfb3355d00f0c43bab82bb17d0f08124f
DESKTOP-00J5IIA\Vuer <289901948@qq.com>
2021-04-09 19:56:32 +0800

    正则表达式学习

--- src/regexp/反向引用.js
+++ src/regexp/反向引用.js
@@ -1,10 +1,9 @@
-
-
-
-
-
-
 // 结果或替换字符串中使用捕获组 (...) 的内容，还可以在模式本身中使用它们。
+// 自己的理解就是使用 刚刚匹配的结果来进行使用
 
+let string = `He said: "She's the one!".`;
 
+// ['"](?<expression>.+)\1 当前的正则失效是因为 没有加() 所以没有拿到捕获组
+let pattern = /(["])(?<expression>[^"]+)\1/g;
 
+console.log(string.match(pattern))
