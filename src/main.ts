8c06c0dcdd5369f40564c65b0d37ae2e77a964e2
DESKTOP-00J5IIA\Vuer <289901948@qq.com>
2021-04-27 19:34:58 +0800

    proxy 学习

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import ElementPlus from "element-plus";
-import "element-plus/lib/theme-chalk/index.css";
-const app = createApp(App);
-app.use(ElementPlus).mount("#app");
-const a = import.meta.globEager("./algorithm/**/*");
-const b = import.meta.globEager("./complier/**/*");
-const c = import.meta.globEager("./dataStructure/**/*");
-const d = import.meta.globEager("./global/**/*");
-const e = import.meta.globEager("./reactive/**/*");
-const f = import.meta.globEager("./regexp/**/*");
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+const app = createApp(App)
+app.use(ElementPlus).mount('#app')
+const a = import.meta.globEager('./algorithm/**/*')
+const b = import.meta.globEager('./complier/**/*')
+const c = import.meta.globEager('./dataStructure/**/*')
+const d = import.meta.globEager('./global/**/*')
+const e = import.meta.globEager('./reactive/**/*')
+const f = import.meta.globEager('./regexp/**/*')
+const g = import.meta.globEager('./proxy/**/*')
 
+/**
+ * [object Object] 名字如何进行修改
+ *
+ */
+
+/**
+ * typeOf Object.protoType.toString.call()
+ * typeOf 无法判断基本包装类型
+ */
