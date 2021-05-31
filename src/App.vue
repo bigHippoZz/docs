220048a8707796f4297a34cf615e75b3b21563b8
bigHippoZz <289901948@qq.com>
2021-05-31 08:44:21 +0800

    feat: vnode diff

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { h } from './h/h'
+// import { h } from './h/h'
 import { defineComponent, reactive, watch, readonly } from 'vue'
 import { TreeNode } from './algorithm/BinaryTreeForEach'
 import HelloWorld from './components/HelloWorld.vue'
@@ -39,25 +39,25 @@ export default defineComponent({
         const data = generateTree([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
         // 旧的 VNode
-        const prevVNode = h('div', null, [
-            h('p', { key: 'a' }, '节点1'),
-            h('p', { key: 'b' }, '节点2'),
-            h('p', { key: 'c' }, '节点3'),
-            h('p', { key: 'd' }, '节点4'),
-            h('p', { key: 'f' }, '节点6'),
-            h('p', { key: 'h' }, '节点8'),
-            h('p', { key: 'e' }, '节点5'),
-        ])
+        // const prevVNode = h('div', null, [
+        //     h('p', { key: 'a' }, '节点1'),
+        //     h('p', { key: 'b' }, '节点2'),
+        //     h('p', { key: 'c' }, '节点3'),
+        //     h('p', { key: 'd' }, '节点4'),
+        //     h('p', { key: 'f' }, '节点6'),
+        //     h('p', { key: 'h' }, '节点8'),
+        //     h('p', { key: 'e' }, '节点5'),
+        // ])
 
         // 新的 VNode
-        const nextVNode = h('div', null, [
-            h('p', { key: 'a' }, 'new 节点1'),
-            h('p', { key: 'c' }, 'new 节点3'),
-            h('p', { key: 'd' }, 'new 节点4'),
-            h('p', { key: 'b' }, 'new 节点2'),
-            h('p', { key: 'g' }, 'new 节点7'),
-            h('p', { key: 'e' }, 'new 节点5'),
-        ])
+        // const nextVNode = h('div', null, [
+        //     h('p', { key: 'a' }, 'new 节点1'),
+        //     h('p', { key: 'c' }, 'new 节点3'),
+        //     h('p', { key: 'd' }, 'new 节点4'),
+        //     h('p', { key: 'b' }, 'new 节点2'),
+        //     h('p', { key: 'g' }, 'new 节点7'),
+        //     h('p', { key: 'e' }, 'new 节点5'),
+        // ])
         // console.log(prevVNode,nextVNode)
 
         // state.a.b.c.name = 'bigHippo'
@@ -81,28 +81,28 @@ export default defineComponent({
         //     }
         // }, 1000)
 
-        const socket = new EnhanceWebSocket(
-            'wss://javascript.info/article/websocket/chat'
-        )
-        console.log(socket)
-        for (let i = 0; i < 10; i++) {
-            socket.send('haha' + i)
-        }
-        socket.openConnection()
-        socket.onmessage(function (event) {
-            console.log(event.data)
-        })
-        setTimeout(() => {
-            socket.send('end')
-            socket.closeConnection()
+        // const socket = new EnhanceWebSocket(
+        //     'wss://javascript.info/article/websocket/chat'
+        // )
+        // console.log(socket)
+        // for (let i = 0; i < 10; i++) {
+        //     socket.send('haha' + i)
+        // }
+        // socket.openConnection()
+        // socket.onmessage(function (event) {
+        //     console.log(event.data)
+        // })
+        // setTimeout(() => {
+        //     socket.send('end')
+        //     socket.closeConnection()
 
-            setTimeout(() => {
-                socket.openConnection()
-                for (let i = 0; i < 10; i++) {
-                    socket.send('gg' + i)
-                }
-            }, 1000)
-        }, 5000)
+        //     setTimeout(() => {
+        //         socket.openConnection()
+        //         for (let i = 0; i < 10; i++) {
+        //             socket.send('gg' + i)
+        //         }
+        //     }, 1000)
+        // }, 5000)
 
         // console.log(cloneDeep(state),state)
     },
--- src/minVue/index.ts
+++ src/minVue/index.ts
@@ -25,7 +25,7 @@ interface ComponentClass {
 type Tag = string | Function | ComponentClassImp | null | Symbol
 export interface VNode {
     _isVNode: true
-    el: Element | null
+    el: Node | null
     flags: VNodeFlags
     tag: Tag
     data: VNodeData | null
@@ -47,30 +47,325 @@ window.onload =
 
         document.body.appendChild(container)
 
+        function mount(vnode: VNode, container: HTMLElement) {
+            const { flags } = vnode
+            if (flags & VNodeFlags.ELEMENT) {
+                // 挂载普通标签
+                mountElement(vnode, container)
+            } else if (flags & VNodeFlags.TEXT) {
+                mountText(vnode, container)
+            } else if (flags & VNodeFlags.FRAGMENT) {
+                mountFragment(vnode, container)
+            } else if (flags & VNodeFlags.COMPONENT) {
+                mountComponent(vnode, container)
+            }
+
+            // else if (flags & VNodeFlags.COMPONENT) {
+            //   // 挂载组件
+            //   mountComponent(vnode, container)
+            // } else if (flags & VNodeFlags.TEXT) {
+            //   // 挂载纯文本
+            //   mountText(vnode, container)
+            // } else if (flags & VNodeFlags.FRAGMENT) {
+            //   // 挂载 Fragment
+            //   mountFragment(vnode, container)
+            // } else if (flags & VNodeFlags.PORTAL) {
+            //   // 挂载 Portal
+            //   mountPortal(vnode, container)
+            // }
+        }
+
         function render(vnode: VNode, container: VElement) {
             const preVNode = container.vnode
-            // if (vnode) {
-            //     if (preVNode) {
-            //         // patch
-            //     } else {
-            //         // mount
-            //     }
-            // } else {
-            //     // remove
-            // }
-            if(preVNode){
-                if(vnode){
-                    
+
+            if (preVNode) {
+                if (vnode) {
+                    patch(preVNode, vnode, container)
+                    vnode.el = container
+                } else {
                 }
-            }else{
-                
+            } else {
+                if (vnode) {
+                    mount(vnode, container)
+                    container.vnode = vnode
+                }
             }
             // mountElement(instance.$vnode, container)
         }
+        function normalizeStyle(val: unknown) {
+            if (isArray(val)) {
+                for (const key of val) {
+                    normalizeStyle(isString(key) ? '' : normalizeStyle(key))
+                }
+            } else if (isObject(val)) {
+            }
+        }
 
+        function mountComponent(vnode: VNode, container: HTMLElement) {
+            // @ts-ignore
+            const instance = (vnode.children = new vnode.tag())
+
+            instance._update = function () {
+                if (instance._mounted) {
+                    const preVNode = instance.$vnode
+                    instance.$props = vnode.data
+                    const nextVNode = (instance.$vnode = instance.render())
+                    patch(preVNode, nextVNode, container)
+                    // @ts-ignore
+                    vnode.el = instance.$el = instance.$vnode.el
+                } else {
+                    instance.$props = vnode.data
+                    instance.$vnode = instance.render()
+                    mount(instance.$vnode, container)
+                    instance._mounted = true
+                    instance.mounted && instance.mounted()
+                    // @ts-ignore
+                    vnode.el = instance.$el = instance.$vnode.el
+                }
+            }
+            instance._update()
+            // instance.$vnode = instance.render()
+            // mount(instance.$vnode, container)
+            // vnode.el = instance.$el = instance.$vnode.el
+        }
+
+        function mountFragment(vnode: VNode, container: HTMLElement) {
+            const { childFlags, children } = vnode
+            switch (childFlags) {
+                case ChildrenFlags.SINGLE_VNODE:
+                    mount(children as VNode, container)
+                    vnode.el = (children as VNode).el
+                    break
+                case ChildrenFlags.KEYED_VNODES:
+                    for (let i = 0; i < (children as []).length; i++) {
+                        mount((children as [])[i], container)
+                    }
+                    // @ts-ignore
+                    vnode.el = children[0].el
+                    break
+                case ChildrenFlags.NO_CHILDREN:
+                    const placeholder = createTextVNode('')
+                    mountText(placeholder, container)
+                    vnode.el = placeholder.el
+                default:
+                    break
+            }
+        }
+        function mountText(vnode: VNode, container: HTMLElement) {
+            const textNode = document.createTextNode(vnode.children as string)
+            vnode.el = textNode
+            container.appendChild(textNode)
+        }
+
+        function patch(preVNode: VNode, vnode: VNode, container: HTMLElement) {
+            const { flags: preFlag } = preVNode
+            const { flags: nextFlag } = vnode
+            if (preFlag !== nextFlag) {
+                replaceVNode(preVNode, vnode, container)
+            } else if (nextFlag & VNodeFlags.ELEMENT) {
+                patchElement(preVNode, vnode, container)
+            } else if (nextFlag & VNodeFlags.TEXT) {
+                patchText(preVNode, vnode)
+            } else if (nextFlag & VNodeFlags.FRAGMENT) {
+                patchFragment(preVNode, vnode, container)
+            } else if (nextFlag & VNodeFlags.COMPONENT) {
+                patchComponent(preVNode, vnode, container)
+            }
+        }
+
+        function patchComponent(
+            prevVNode: VNode,
+            nextVNode: VNode,
+            container: HTMLElement
+        ) {
+            if (nextVNode.tag !== prevVNode.tag) {
+                replaceVNode(prevVNode, nextVNode, container)
+            } else if (nextVNode.flags & VNodeFlags.COMPONENT_STATEFUL) {
+                const instance = (nextVNode.children = prevVNode.children)
+                // @ts-ignore
+                instance.$props = nextVNode.data
+                // @ts-ignore
+                instance._update()
+            }
+        }
+
+        function patchFragment(
+            preVNode: VNode,
+            nextVNode: VNode,
+            container: HTMLElement
+        ) {
+            patchChildren(
+                preVNode.childFlags,
+                nextVNode.childFlags,
+                preVNode,
+                nextVNode,
+                container
+            )
+
+            switch (nextVNode.childFlags) {
+                case ChildrenFlags.SINGLE_VNODE:
+                    nextVNode.el = (nextVNode.children as VNode).el
+                    break
+                case ChildrenFlags.NO_CHILDREN:
+                    nextVNode.el = preVNode.el
+                    break
+                default:
+                    // @ts-ignore
+                    nextVNode.el = nextVNode.children[0].el
+                    break
+            }
+        }
+        function patchText(preVNode: VNode, nextVNode: VNode) {
+            const el = (nextVNode.el = preVNode.el)
+            if (preVNode.children !== nextVNode.children) {
+                if (el?.nodeValue) {
+                    el.nodeValue = nextVNode.children as string
+                }
+            }
+        }
+
+        function patchData(
+            el: HTMLElement,
+            key: string,
+            preValue: any,
+            nextValue: any
+        ) {
+            switch (key) {
+                case 'style':
+                    for (const key in nextValue) {
+                        // @ts-ignore
+                        el.style[key] = nextValue[key]
+                    }
+                    for (const key in preValue) {
+                        // @ts-ignore
+                        if (!nextValue.hasOwnProperty(key)) {
+                            // @ts-ignore
+                            el.style[key] = ''
+                        }
+                    }
+                    break
+                case 'class':
+                    el.className = nextValue
+                default:
+                    if (key.startsWith('on')) {
+                        if (preValue) {
+                            el.removeEventListener(key.slice(2), preValue)
+                        }
+
+                        if (nextValue) {
+                            el.addEventListener(key.slice(2), nextValue)
+                        }
+                    } else if (domPropsRE.test(key)) {
+                        // domProps
+                        // @ts-ignore
+                        el[key] = nextValue
+                    } else {
+                        // dom attr
+                        el.setAttribute(key, nextValue)
+                    }
+
+                    break
+            }
+        }
+
+        function patchElement(
+            preVNode: VNode,
+            vnode: VNode,
+            container: HTMLElement
+        ) {
+            const { tag: preTag, data: preData } = preVNode
+            const { tag: nextTag, data: nextData } = vnode
+
+            if (preTag !== nextTag) {
+                replaceVNode(preVNode, vnode, container)
+                return
+            }
+            const el = (vnode.el = preVNode.el)
+            if (nextData) {
+                for (const key in nextData) {
+                    const preVNodeValue = preData![key]
+                    const nextVNodeValue = nextData[key]
+                    patchData(
+                        el as HTMLElement,
+                        key,
+                        preVNodeValue,
+                        nextVNodeValue
+                    )
+                }
+            }
+
+            if (preData) {
+                for (const key in preData) {
+                    const preVNodeValue = preData![key]
+                    if (preVNodeValue && !nextData!.hasOwnProperty(key)) {
+                        patchData(el as HTMLElement, key, preVNodeValue, null)
+                    }
+                }
+            }
+
+            patchChildren(
+                preVNode.childFlags,
+                vnode.childFlags,
+                preVNode.children,
+                vnode.children,
+                el as HTMLElement
+            )
+        }
+        function replaceVNode(
+            preVNode: VNode,
+            vnode: VNode,
+            container: HTMLElement
+        ) {
+            container.removeChild(preVNode.el!)
+            if(preVNode.flags & VNodeFlags.COMPONENT_STATEFUL){
+                const instance = preVNode.children
+                // @ts-ignore
+                instance.unMounted&&instance.unMounted()
+            }
+            mount(vnode, container)
+        }
+        const domPropsRE = /\[A-Z]|^(?:value|checked|selected|muted)$/
         function mountElement(vnode: VNode, container: VElement) {
+            const { childFlags, children, data } = vnode
             // 创建元素
             const el = document.createElement(vnode.tag as string)
+            // vnode 引用el
+            vnode.el = el
+            if (data) {
+                for (const key in data) {
+                    switch (key) {
+                        case 'style':
+                            for (const key in data.style) {
+                                // @ts-ignores
+                                el.style[key] = data.style[key]
+                            }
+                            break
+                        case 'class':
+                            el.className = data[key]
+                            break
+                        default:
+                            if (key.startsWith('on')) {
+                                el.addEventListener(key.slice(2), data[key])
+                            } else if (domPropsRE.test(key)) {
+                                // @ts-ignore
+                                el[key] = data[key]
+                            } else {
+                                el.setAttribute(key, data[key])
+                            }
+
+                            break
+                    }
+                }
+            }
+            if (childFlags !== ChildrenFlags.NO_CHILDREN) {
+                if (childFlags & ChildrenFlags.SINGLE_VNODE) {
+                    mount(children as VNode, el)
+                } else if (childFlags & ChildrenFlags.MULTIPLE_VNODES) {
+                    for (let i = 0; i < (children as []).length; i++) {
+                        mount((children as [])[i], el)
+                    }
+                }
+            }
             // 将元素添加到容器
             container.appendChild(el)
         }
@@ -163,9 +458,143 @@ window.onload =
                 childFlags: ChildrenFlags.NO_CHILDREN,
                 el: null,
             }
+        }       
+
+        function patchChildren(
+            prevChildFlags: number,
+            nextChildFlags: number,
+            prevChildren: VNode[] | null | string | VNode,
+            nextChildren: VNode[] | null | string | VNode,
+            container: HTMLElement
+        ) {
+            switch (prevChildFlags) {
+                case ChildrenFlags.SINGLE_VNODE:
+                    switch (nextChildFlags) {
+                        case ChildrenFlags.SINGLE_VNODE:
+                            patch(
+                                prevChildren as VNode,
+                                nextChildren as VNode,
+                                container
+                            )
+                            break
+                        case ChildrenFlags.NO_CHILDREN:
+                            container.removeChild((prevChildren as VNode).el!)
+                            break
+                        default:
+                            container.removeChild((prevChildren as VNode).el!)
+                            for (
+                                let i = 0;
+                                i < (nextChildren as []).length;
+                                i++
+                            ) {
+                                mount((nextChildren as [])[i], container)
+                            }
+                            break
+                    }
+                    break
+                case ChildrenFlags.NO_CHILDREN:
+                    switch (nextChildFlags) {
+                        case ChildrenFlags.SINGLE_VNODE:
+                            mount(nextChildren as VNode, container)
+                            break
+                        case ChildrenFlags.NO_CHILDREN:
+                            break
+                        default:
+                            for (
+                                let i = 0;
+                                i < (nextChildren as []).length;
+                                i++
+                            ) {
+                                mount((nextChildren as [])[i], container)
+                            }
+                            break
+                    }
+                    break
+                default:
+                    switch (nextChildFlags) {
+                        case ChildrenFlags.SINGLE_VNODE:
+                            for (
+                                let i = 0;
+                                i < (prevChildren as []).length;
+                                i++
+                            ) {
+                                container.removeChild(
+                                    (prevChildren as VNode[])[i].el!
+                                )
+                            }
+                            mount(nextChildren as VNode, container)
+                            break
+                        case ChildrenFlags.NO_CHILDREN:
+                            for (
+                                let i = 0;
+                                i < (prevChildren as []).length;
+                                i++
+                            ) {
+                                container.removeChild(
+                                    (prevChildren as VNode[])[i].el!
+                                )
+                            }
+                            break
+                        default:
+                            break
+                    }
+                    break
+            }
         }
 
-        const fragmentVNode = h(Fragment, {}, [h('h1'), h('h1')])
+        const fragmentVNode = h(Fragment, null, [h('h1')])
 
-        console.log(fragmentVNode)
+        class Header extends Component {
+            state = 'hello world'
+
+            mounted() {
+                setTimeout(() => {
+                    this.state = 'hello bigHippo'
+                    // @ts-ignore
+                    this._update()
+                }, 1000)
+            }
+
+            render() {
+                return h(
+                    'div',
+                    {
+                        style: {
+                            height: '100px',
+                            width: '100px',
+                            background: 'red',
+                        },
+                        onclick: () => {
+                            // @ts-ignore
+                            console.log(this.$props.name)
+                        },
+                    },
+                    this.state
+                )
+            }
+        }
+
+        class Container extends Component {
+            render() {
+                return h(Header, { name: 'liwuzhou' })
+            }
+        }
+
+        // // 旧的 VNode
+        // const prevVNode = h(Fragment, null, [
+        //     h('p', null, '旧片段子节点 1'),
+        //     h('p', null, '旧片段子节点 2'),
+        // ])
+
+        // // 新的 VNode
+        // const nextVNode = h(Fragment, null, [
+        //     h('p', null, '新片段子节点 1'),
+        //     h('p', null, '新片段子节点 2'),
+        // ])
+
+        render(h(Container), container)
+
+        // setTimeout(() => render(nextVNode, container), 1000)
+        // console.log(container)
+        // console.log(h(Header))
     }
