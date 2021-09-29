7339a50b0c94731db5181dc124bbb3ec122c6799
李武周 <289901948@qq.com>
2021-09-29 15:07:46 +0800

    refactor: update`StopWatch`

--- src/utils/stopWatch.ts
+++ src/utils/stopWatch.ts
@@ -1,8 +1,9 @@
+export const defaultPerformance = window.performance;
+
 export class StopWatch {
-  protected _execution = window.performance;
+  protected _execution = defaultPerformance;
   protected _startTime = -1;
   protected _runTime!: number;
-  constructor() {}
 
   public stop() {
     this._startTime = this._now();
@@ -16,10 +17,21 @@ export class StopWatch {
     this._startTime = -1;
     return this._runTime;
   }
+
   get runTime(): number {
     return this._runTime;
   }
+
   protected _now(): number {
     return this._execution.now();
   }
 }
+
+/**
+ *
+ * const stopWatch = new StopWatch();
+ * stopWatch.stop()
+ * console.log("hello world")
+ * stopWatch.elapsed()
+ *
+ */
