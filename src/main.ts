561541ce516b126dc9e48d676ebaaf99fe433348
李武周 <289901948@qq.com>
2021-09-29 15:25:16 +0800

    refactor: delete http folder

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { setupAppRouter } from "./routers";
-
 const app = createApp(App);
 setupAppRouter(app);
 app.mount("#app");
-
-// class TreeNode {
-//   val: number;
-//   left: TreeNode | null;
-//   right: TreeNode | null;
-//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-//     this.val = val === undefined ? 0 : val;
-//     this.left = left === undefined ? null : left;
-//     this.right = right === undefined ? null : right;
-//   }
-// }
-
-// function pathSum(root: TreeNode | null, targetSum: number): number {
-//   let ans = 0;
-
-//   function helper(root: TreeNode | null) {
-//     if (!root) return 0;
-//     const left = helper(root.left);
-//     const right = helper(root.right);
-//     if (left === targetSum) ans++;
-//     if (right === targetSum) ans++;
-//   }
-//   return ans;
-// }
