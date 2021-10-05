c61da6633e4b79d6df4a56e3bac87d650c7f69e6
李武周 <289901948@qq.com>
2021-10-05 22:08:18 +0800

    feat(utils): add `randomString`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,3 +15,27 @@ export function isSorted(arr: Array<any>, i?: number, j?: number): boolean {
   }
   return true;
 }
+
+/**
+ * 返回测试数据
+ * @param fileName string
+ * @returns
+ */
+export const fetchData = async (fileName: string) => {
+  const response = await fetch(`./data/${fileName}`);
+  return await response.text();
+};
+
+/**
+ * 生成随机字符串
+ */
+export const randomString = (num: number) => {
+  num = num || 32;
+  const string = "abcdefhijkmnprstwxyz12345678";
+  const len = string.length;
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += string.charAt((Math.random() * len) >>> 0);
+  }
+  return result;
+};
