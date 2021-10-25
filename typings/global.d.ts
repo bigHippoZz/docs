2e07ab9533031d3b9c6a40e86d4dd86f1231b022
李武周 <289901948@qq.com>
2021-10-25 14:56:35 +0800

    types: add some types

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,27 +1,27 @@
 import type { ComponentPublicInstance, FunctionalComponent } from "vue";
 import type { Ref } from "vue";
 declare global {
-  const __DEV__: boolean;
-  const __APP_INFO__: string;
-  const PERFORM_SORTING: boolean;
+	const __DEV__: boolean;
+	const __APP_INFO__: string;
+	const PERFORM_SORTING: boolean;
 
-  declare interface ViteEnv {
-    VITE_PUBLIC_PATH: string;
-    VITE_PROXY: [string, string][];
-    VITE_DROP_CONSOLE: boolean;
-    VITE_GLOB_API_URL: string;
-    VITE_GLOB_UPLOAD_URL: string;
-    VITE_GLOB_API_URL_PREFIX: string;
-    VITE_PORT: number;
-  }
+	declare interface ViteEnv {
+		VITE_PUBLIC_PATH: string;
+		VITE_PROXY: [string, string][];
+		VITE_DROP_CONSOLE: boolean;
+		VITE_GLOB_API_URL: string;
+		VITE_GLOB_UPLOAD_URL: string;
+		VITE_GLOB_API_URL_PREFIX: string;
+		VITE_PORT: number;
+	}
 
-  declare type Nullable<T> = T | null;
-  declare type Recordable<T = any> = Record<string, T>;
-  declare type MaybeRef<T> = T | Ref<T>;
+	declare type Nullable<T> = T | null;
+	declare type Recordable<T = any> = Record<string, T>;
+	declare type MaybeRef<T> = T | Ref<T>;
 }
 
 declare module "vue" {
-  export type JSXComponent<Props = any> =
-    | { new (): ComponentPublicInstance<Props> }
-    | FunctionalComponent<Props>;
+	export type JSXComponent<Props = any> =
+		| { new (): ComponentPublicInstance<Props> }
+		| FunctionalComponent<Props>;
 }
---
+++ typings/utils.d.ts
@@ -0,0 +1,8 @@
+declare type Primitive =
+	| number
+	| string
+	| boolean
+	| bigint
+	| symbol
+	| null
+	| undefined;
